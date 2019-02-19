e906c8b97049aff1d0f8a153f8145984f9001c87
Codie <black.nova.13.5@gmail.com>
2019-02-19 09:52:50 -0700

    Update LSTM_class.py

--- LSTM_class.py
+++ LSTM_class.py
@@ -4,13 +4,12 @@ import torch
    parameters for chorales."""
 
 class LSTM(torch.nn.Module):
-    def __init__(self, num_samples, input_size=88, hidden_size=20,
-                 output_size=88, num_layers=1, bidirectional=False):
+    def __init__(self, input_size=88, hidden_size=88, output_size=88, num_layers=1, 
+                 bidirectional=False):
         super().__init__() # Run Module class constructor
         self.input_size = input_size # Number of expected features in the input
         self.hidden_size = hidden_size # Number of features in hidden state
         self.output_size = output_size # Number of expected features in the output
-        self.num_samples = num_samples # (this changes based on song length. we might not be able to hardcode it)
         self.num_layers = num_layers # number of LSTM blocks to 'stack'
         self.bidirectional = bidirectional
 
@@ -34,20 +33,20 @@ class LSTM(torch.nn.Module):
         return (torch.zeros(self.num_layers, 1, self.hidden_size),
                 torch.zeros(self.num_layers, 1, self.hidden_size))
 
-    def forward(self, input):
+    def forward(self, input, num_samples):
         """
         Runs input data once through the network.
         """
         # View reshapes the input into the expected size
         # Probably should be done outside of the class
-        input = input.view(self.input_size, self.num_samples,
+        input = input.view(self.input_size, num_samples,
                            self.hidden_size)
 
         # Send input through hidden LSTM layers
         out, self.hidden = self.lstm(input, self.hidden)
 
         # Convert output of LSTM layers into final output prediction
-        pred = self.linear(out.view(self.output_size, self.num_samples,
+        pred = self.linear(out.view(self.output_size, num_samples,
                                     self.hidden_size))
 
         # Return prediction of size output_size
