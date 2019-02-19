933f9356ef58433a755c0d315c88a1993d92a589
Codie <black.nova.13.5@gmail.com>
2019-02-19 09:28:26 -0700

    Update LSTM_class.py

--- LSTM_class.py
+++ LSTM_class.py
@@ -4,11 +4,11 @@ import torch
    parameters for chorales."""
 
 class LSTM(torch.nn.Module):
-    def __init__(self, num_samples, input_size=88, num_nodes=20,
+    def __init__(self, num_samples, input_size=88, hidden_size=20,
                  output_size=88, num_layers=1, bidirectional=False):
         super().__init__() # Run Module class constructor
         self.input_size = input_size # Number of expected features in the input
-        self.num_nodes = num_nodes # Number of features in hidden state
+        self.hidden_size = hidden_size # Number of features in hidden state
         self.output_size = output_size # Number of expected features in the output
         self.num_samples = num_samples # (this changes based on song length. we might not be able to hardcode it)
         self.num_layers = num_layers # number of LSTM blocks to 'stack'
@@ -16,7 +16,7 @@ class LSTM(torch.nn.Module):
 
         # Define LSTM layers using pytorch built-in
         self.lstm = torch.nn.LSTM(input_size = self.input_size,
-                                  hidden_size = self.num_nodes,
+                                  hidden_size = self.hidden_size,
                                   num_layers = self.num_layers,
                                   bias = True,
                                   batch_first = False,
@@ -24,15 +24,15 @@ class LSTM(torch.nn.Module):
                                   bidirectional = self.bidirectional)
 
         # Define the final transformation from the hidden layers to output
-        self.linear = torch.nn.Linear(self.num_nodes, output_size)
+        self.linear = torch.nn.Linear(self.hidden_size, output_size)
 
         # Initialize hidden weights
         self.hidden = self.init_hidden()
 
     def init_hidden(self):
-        # The axes are (num_layers, minibatch_size, hidden_dim)
-        return (torch.zeros(self.num_layers, 1, self.hidden_dim),
-                torch.zeros(self.num_layers, 1, self.hidden_dim))
+        # The axes are (num_layers, minibatch_size, hidden_size)
+        return (torch.zeros(self.num_layers, 1, self.hidden_size),
+                torch.zeros(self.num_layers, 1, self.hidden_size))
 
     def forward(self, input):
         """
@@ -41,14 +41,14 @@ class LSTM(torch.nn.Module):
         # View reshapes the input into the expected size
         # Probably should be done outside of the class
         input = input.view(self.input_size, self.num_samples,
-                           self.num_nodes)
+                           self.hidden_size)
 
         # Send input through hidden LSTM layers
         out, self.hidden = self.lstm(input, self.hidden)
 
         # Convert output of LSTM layers into final output prediction
         pred = self.linear(out.view(self.output_size, self.num_samples,
-                                    self.num_nodes))
+                                    self.hidden_size))
 
         # Return prediction of size output_size
         return pred
